f9331f14f4123e84ffe8f6a5a3c7c78139f5b5cb
Joy Lin <vvlnote@hotmail.com>
2019-04-29 02:51:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ class App < Sinatra::Base
   post '/team' do
     @team_name = params[:name]
     @coach = params[:coach]
-    @point_guard = params[:point_guard]
-    @shooting_guard = params[:shooting_guard]
-    @small_forward = params[:small_forward]
-    @power_forward = params[:power_forward]
-    @center = params[:center]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
+    @center = params[:c]
     erb :team
   end
   
